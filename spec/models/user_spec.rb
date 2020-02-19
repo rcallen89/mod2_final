8ae8c7a7529f6a05033fb6c4471f2fc7405fc1d3
Ryan Allen <rcallen89@gmail.com>
2020-02-19 07:57:21 -0700

    Add User model test for Enum and Roles

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,39 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of :email }
     it { should validate_presence_of :password }
   end
+
+  describe 'role enum' do
+    it 'should list proper roles for enum' do
+      user_1 = User.create(name: "Benny",
+                          street_address: "123 Main Street",
+                          city: "CO",
+                          state: "Denver",
+                          zip: "80144",
+                          role: 0,
+                          email: "test@example.com",
+                          password: "password")
+
+      user_2 = User.create(name: "Benny",
+                          street_address: "123 Main Street",
+                          city: "CO",
+                          state: "Denver",
+                          zip: "80144",
+                          role: 1,
+                          email: "test1@example.com",
+                          password: "password")
+
+      user_3 = User.create(name: "Benny",
+                          street_address: "123 Main Street",
+                          city: "CO",
+                          state: "Denver",
+                          zip: "80144",
+                          role: 2,
+                          email: "test3@example.com",
+                          password: "password")
+
+      expect(user_1.role).to eq("User")
+      expect(user_2.role).to eq("MerchantEmployee")
+      expect(user_3.role).to eq("Admin")
+    end
+  end
 end
