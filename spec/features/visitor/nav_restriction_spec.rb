8141e3de6e2b2538907152ac92c6a6487d63a37a
Jordan Sewell <jrsewell400@gmail.com>
2020-02-19 13:53:39 -0700

    Create test to ensure visitor cannot visit admin, profile, and merchant_employee pages

--- spec/features/visitor/nav_restriction_spec.rb
+++ spec/features/visitor/nav_restriction_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "As a visitor," do
+RSpec.describe "As a visitor,", type: :feature do
   context "when I try to access a restriced path" do
     it "then I see a 404 error." do
-      visit '/merchant/'
+      visit '/merchant'
+      expect(page).to have_content("The page you were looking for doesn't exist.")
 
-      expect(page).to have_content("")
+      visit '/admin'
+      expect(page).to have_content("The page you were looking for doesn't exist.")
+
+      visit '/profile'
+      expect(page).to have_content("The page you were looking for doesn't exist.")
     end
   end
 end
