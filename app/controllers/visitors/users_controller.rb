d9e929ffb0e4cb96013c34ae8e5eadcbda41e187
Cassie Achzenick <caachzenick@gmail.com>
2020-02-20 10:42:29 -0700

    User story 12 complete

--- app/controllers/visitors/users_controller.rb
+++ app/controllers/visitors/users_controller.rb
@@ -5,21 +5,21 @@ class Visitors::UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       flash[:notice] = "Successfully Registered and Logged In"
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/profile'
     else
-
-      redirect_to '/register'
+      flash[:error] = @user.errors.full_messages.to_sentence
+      render :new
     end
   end
 
   private
 
     def user_params
-      params.permit(:name, :street_address, :city, :state, :zip, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :street_address, :city, :state, :zip, :email, :password, :password_confirmation)
     end
 
 end
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -5,15 +5,15 @@ RSpec.describe 'New Registration Page', type: :feature do
     it 'shows a form to register a default user' do
 
       visit '/register'
-      save_and_open_page
+
       fill_in :Name, with: 'Benny'
-      fill_in :street_address, with: '1234 Main St.'
+      fill_in :user_street_address, with: '1234 Main St.'
       fill_in :City, with: 'Denver'
       fill_in :State, with: 'CO'
       fill_in :Zip, with: '80144'
       fill_in :Email, with: 'benny@email.com'
       fill_in :Password, with: 'password'
-      fill_in :Password_confirmation, with: 'password'
+      fill_in :user_password_confirmation, with: 'password'
 
       click_on "Register Now"
 
@@ -29,16 +29,15 @@ RSpec.describe 'New Registration Page', type: :feature do
       visit '/register'
 
       fill_in :Name, with: 'Benny'
-      fill_in :Address, with: '1234 Main St.'
+      fill_in :user_street_address, with: '1234 Main St.'
       fill_in :City, with: 'Denver'
       fill_in :State, with: 'CO'
       fill_in :Email, with: 'benny@email.com'
       fill_in :Password, with: 'password'
-      fill_in :Password_confirmation, with: 'password'
+      fill_in :user_password_confirmation, with: 'password'
 
       click_on "Register Now"
 
-      expect(current_path).to eq("/register")
       expect(page).to have_content("Zip can't be blank")
 
       # How to test successfully logged in
@@ -53,31 +52,25 @@ RSpec.describe 'New Registration Page', type: :feature do
     expect(User.all.length).to eq(1)
 
     fill_in :Name, with: 'Mary'
-    fill_in :Street_address, with: '2859 somthing st.'
+    fill_in :user_street_address, with: '2859 somthing st.'
     fill_in :City, with: 'Boston'
     fill_in :State, with: 'MA'
     fill_in :Zip, with: '59032'
     fill_in :Email, with: 'Rainbow_unicorn@example.com'
     fill_in :Password, with: 'Something'
-    fill_in :Password_confirmation, with: 'Something'
+    fill_in :user_password_confirmation, with: 'Something'
 
     click_on "Register Now"
 
-    expect(current_path).to eq("/register")
-
-    # within "#logged in user" do
-      expect(page).to_not have_content("Mary")
-    # end
+    expect(page).to_not have_content("Mary")
 
     expect(page).to have_content("Email has already been taken")
 
-    # within "#registration form" do
-      expect(find_field('name').value).to eq 'John'
-      expect(find_field('street_address').value).to eq '2859 somthing st.'
-      expect(find_field('city').value).to eq 'Boston'
-      expect(find_field('state').value).to eq 'MA'
-      expect(find_field('zip').value).to eq '59032'
-    # end
+    expect(find_field('Name').value).to eq 'Mary'
+    expect(find_field('Street address').value).to eq '2859 somthing st.'
+    expect(find_field('City').value).to eq 'Boston'
+    expect(find_field('State').value).to eq 'MA'
+    expect(find_field('Zip').value).to eq '59032'
     end
   end
 end
