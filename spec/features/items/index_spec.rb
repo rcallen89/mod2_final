97bd4ce393ea71ba6acead4c551d08b6c75d6f8e
Ryan Allen <rcallen89@gmail.com>
2020-02-20 16:01:08 -0700

    Add User Story 17

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -12,18 +12,17 @@ RSpec.describe "Items Index Page" do
       @dog_bone = @brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
     end
 
-    it "all items or merchant names are links" do
+    it "all items or merchant names are links without active items" do
       visit '/items'
 
       expect(page).to have_link(@tire.name)
       expect(page).to have_link(@tire.merchant.name)
       expect(page).to have_link(@pull_toy.name)
       expect(page).to have_link(@pull_toy.merchant.name)
-      expect(page).to have_link(@dog_bone.name)
-      expect(page).to have_link(@dog_bone.merchant.name)
+
     end
 
-    it "I can see a list of all of the items "do
+    it "I can see a list of all active items "do
 
       visit '/items'
 
@@ -47,15 +46,12 @@ RSpec.describe "Items Index Page" do
         expect(page).to have_css("img[src*='#{@pull_toy.image}']")
       end
 
-      within "#item-#{@dog_bone.id}" do
-        expect(page).to have_link(@dog_bone.name)
-        expect(page).to have_content(@dog_bone.description)
-        expect(page).to have_content("Price: $#{@dog_bone.price}")
-        expect(page).to have_content("Inactive")
-        expect(page).to have_content("Inventory: #{@dog_bone.inventory}")
-        expect(page).to have_link(@brian.name)
-        expect(page).to have_css("img[src*='#{@dog_bone.image}']")
-      end
+    end
+
+    it 'shouldnt show inactive items' do
+      visit '/items'
+
+      expect(page).to_not have_content(@dog_bone.name)
     end
   end
 end
