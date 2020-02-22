5dda6941069170e743d083f6154fdc5df0816ff9
Cassie Achzenick <caachzenick@gmail.com>
2020-02-22 10:35:53 -0700

    Add user story 24 model test

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -107,4 +107,34 @@ RSpec.describe Cart do
       expect(cart.add_quantity(item2)).to eq(1)
     end
   end
+
+  describe "#subtract_quantity" do
+    it "decrements the quantity of an item already in your cart" do
+      merchant1 = Merchant.create!(name: "Bunny Rabbit Toys", address: '1234 nothing st', city: "Nowhere", state: "OK", zip: "48993")
+      item1 = Item.create!(name: "Toy", description: "bright and colorful", price: 20, inventory: 3, merchant: merchant1, image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1564437457-71fdrxaapvl-sl1500-1564437437.jpg")
+      item2 = Item.create!(name: "Stuffed Animal", description: "Soft and Cuddly", price: 30, inventory: 12, merchant: merchant1, image: "https://www.potterybarnkids.com/products/grey-hippo-nursery-plush-collection/?catalogId=10&sku=3188538&cm_ven=PLA&cm_cat=Google&cm_pla=Toys%20%26%20Play%20%3E%20Rockers%20%26%20Plush%20Toys%20%3E%20Plush%20Toys&cm_ite=3188538&gclid=Cj0KCQiAv8PyBRDMARIsAFo4wK2dZw7bpQDozFrmoLKpp9n6SKcvHR4Rx8wl0r4e7u0cw66VTdzlZicaArlfEALw_wcB#viewLargerSubsetOverlay")
+
+      cart = Cart.new(Hash.new(0))
+
+      cart.add_item(item2.id.to_s)
+      cart.add_quantity(item2)
+      cart.add_quantity(item2)
+
+      expect(cart.subtract_quantity(item2)).to eq(1)
+    end
+  end
+
+  describe "#quantity_zero?" do
+    it "checks if the quantity in the cart is 0" do
+      merchant1 = Merchant.create!(name: "Bunny Rabbit Toys", address: '1234 nothing st', city: "Nowhere", state: "OK", zip: "48993")
+      item1 = Item.create!(name: "Toy", description: "bright and colorful", price: 20, inventory: 3, merchant: merchant1, image: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1564437457-71fdrxaapvl-sl1500-1564437437.jpg")
+      item2 = Item.create!(name: "Stuffed Animal", description: "Soft and Cuddly", price: 30, inventory: 12, merchant: merchant1, image: "https://www.potterybarnkids.com/products/grey-hippo-nursery-plush-collection/?catalogId=10&sku=3188538&cm_ven=PLA&cm_cat=Google&cm_pla=Toys%20%26%20Play%20%3E%20Rockers%20%26%20Plush%20Toys%20%3E%20Plush%20Toys&cm_ite=3188538&gclid=Cj0KCQiAv8PyBRDMARIsAFo4wK2dZw7bpQDozFrmoLKpp9n6SKcvHR4Rx8wl0r4e7u0cw66VTdzlZicaArlfEALw_wcB#viewLargerSubsetOverlay")
+
+      cart = Cart.new(Hash.new(0))
+
+      cart.add_item(item2.id.to_s)
+
+      expect(cart.quantity_zero?(item2)).to be true
+    end
+  end
 end
