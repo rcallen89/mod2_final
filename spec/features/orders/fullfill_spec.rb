4b2ca86d71ed991e671cda4693af348395d10f50
Cassie Achzenick <caachzenick@gmail.com>
2020-02-24 11:57:47 -0700

    Remove commented out test from another user story

--- spec/features/orders/fullfill_spec.rb
+++ spec/features/orders/fullfill_spec.rb
@@ -97,36 +97,4 @@ RSpec.describe("Order Fullfillment") do
       expect(page).to have_content("Item cannot be fulfilled")
     end
   end
-
-# #   it "changes order status to packaged when all orders have been fulfilled" do
-    # @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
-    # @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-    # @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-    # @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
-    # @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
-    #
-    # visit "/items/#{@paper.id}"
-    # click_on "Add To Cart"
-    # visit "/items/#{@paper.id}"
-    # click_on "Add To Cart"
-    # visit "/items/#{@tire.id}"
-    # click_on "Add To Cart"
-    # visit "/items/#{@pencil.id}"
-    # click_on "Add To Cart"
-    #
-    # visit "/cart"
-    #
-    # click_on "Checkout"
-    #
-    # fill_in :name, with: "Cassie"
-    # fill_in :address, with: "1234 nothing st"
-    # fill_in :city, with: "Nowhere"
-    # fill_in :state, with: "BA"
-    # fill_in :zip, with: "89874"
-    # click_on("Create Order")
-#
-#
-# #     When all items in an order have been "fulfilled" by their merchants
-# # The order status changes from "pending" to "packaged"
-#   end
 end
