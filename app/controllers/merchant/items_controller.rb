ba9c9bb1e6dae28e8e8a6f9961a3743380704107
Ryan Allen <rcallen89@gmail.com>
2020-02-25 11:08:21 -0700

    Create Merchant Item show page Create Item Active/Deactive buttons and method Create Tests for deactivating a item

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -1,4 +1,18 @@
 class Merchant::ItemsController < Merchant::BaseController 
+
     def index
+      @items = current_user.merchants.first.items
     end 
+
+    def update
+      item = Item.find(params[:id])
+      item.toggle(:active?)
+      item.save
+      if item.active?
+        flash[:notice] = "#{item.name} now available for sale"
+      else
+        flash[:notice] = "#{item.name} is no longer for sale"
+      end
+      redirect_to '/merchant/items'
+    end
 end
