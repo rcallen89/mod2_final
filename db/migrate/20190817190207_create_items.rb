5e5d9595b7e03cde06b0b9eb7e5e8768d7109c69
Ryan Allen <rcallen89@gmail.com>
2020-02-26 10:46:53 -0700

    Modify Item Controller for New and Edit form_for Modify Item Table for default image Modify Item Table for float

--- db/migrate/20190817190207_create_items.rb
+++ db/migrate/20190817190207_create_items.rb
@@ -3,8 +3,8 @@ class CreateItems < ActiveRecord::Migration[5.1]
     create_table :items do |t|
       t.string :name
       t.string :description
-      t.integer :price
-      t.string :image
+      t.float :price
+      t.string :image, default: "https://www.wpclipart.com/office/sale_promo/new_item/new_item_light_red.png"
       t.boolean :active?, default: true
       t.integer :inventory
       t.references :merchant, foreign_key: true
