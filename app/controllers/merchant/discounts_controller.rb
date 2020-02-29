2fae2aeb0a4fc005f27b52a88f0d77b60347eb69
Ryan Allen <rcallen89@gmail.com>
2020-02-28 20:53:53 -0700

    Create Discount Create Method

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -4,5 +4,18 @@ class Merchant::DiscountsController < Merchant::BaseController
     @discount = Discount.new
   end
 
+  def create
+    merchant = Merchant.find(current_user.merchants.first.id)
+    merchant.discounts.create(discount_params)
+    flash[:notice] = 'Discount Created'
+    redirect_to '/merchant'
+  end
+
+  private
+
+  def discount_params
+    params.require(:discount).permit(:percentage, :per_item)  
+  end
+
 
 end
