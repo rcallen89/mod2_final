af79caeb49b82a0069d5cde6a90a7c6af36049da
Ryan Allen <rcallen89@gmail.com>
2020-02-28 20:53:38 -0700

    Create PORO for displaying merchant info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart, :current_user, :role, :employee?, :has_rights?
+  helper_method :cart, :current_user, :role, :employee?, :has_rights? 
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
