3659b3a920a3a437972bc2fca70668c55933b346
Ryan Allen <rcallen89@gmail.com>
2020-03-02 14:00:21 -0700

    Add Item Model Spec Testing

--- spec/features/cart/bulk_discount_spec.rb
+++ spec/features/cart/bulk_discount_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Merchant Bulk Discount', type: :feature do
     @user = create(:user)
     @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
     @bike_shop.discounts.create!(percentage: 10, per_item: 2)
+    @bike_shop.discounts.create!(percentage: 20, per_item: 4)
     @tire = @bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 25)
     @chain = @bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 100, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 15)
     @shifter = @bike_shop.items.create(name: "Shimano Shifters", description: "It'll always shift!", price: 100, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 10)
@@ -31,6 +32,23 @@ RSpec.describe 'Merchant Bulk Discount', type: :feature do
     end
 
     expect(page).to have_content("Total: $380.00")
+  end
+
+  it 'will apply the biggest discount if there is more than one' do
+    visit '/cart'
+
+    within "#cart-item-#{@tire.id}" do
+      click_on "Add Quantity"
+      click_on "Add Quantity"
+      click_on "Add Quantity"
+    end
+
+    within "#cart-item-#{@tire.id}" do
+      expect(page).to have_content("$320.00")
+      expect(page).to have_content("20% off for bulk discount!")
+    end
+
+    expect(page).to have_content("Total: $520.00")
   end
 end
 
